7ea72f2d6ef93b8e92f34948f4bdd9a822a39f93
choilm <choilmto@gmail.com>
2020-11-01 08:40:07 -0500

    Setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
-    env: {
-        node: true,
-        commonjs: true,
-        es2021: true,
-    },
-    extends: ["eslint:recommended", "plugin:yaml/recommended"],
-    parserOptions: {
-        ecmaVersion: 12,
-    },
-    rules: {},
-    plugins: [
-        "yaml"
-    ],
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:yaml/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+  plugins: ["yaml"],
 };
