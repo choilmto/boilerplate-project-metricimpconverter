6429c9b9ce68c4a13336cd771f05dbfc253f36a9
choilm <choilmto@gmail.com>
2020-11-01 08:40:07 -0500

    Setup prettier

--- logger.js
+++ logger.js
@@ -2,15 +2,14 @@ const pino = require("pino");
 const { createWriteStream } = require("pino-logflare");
 
 const connectPino = () => {
-    const stream = createWriteStream({
-        apiKey: process.env.LOGFLARE_API_KEY,
-        sourceToken: process.env.LOGFLARE_SOURCE_TOKEN,
-    });
-    const options = {};
-    return pino(options, stream);
-}
+  const stream = createWriteStream({
+    apiKey: process.env.LOGFLARE_API_KEY,
+    sourceToken: process.env.LOGFLARE_SOURCE_TOKEN,
+  });
+  const options = {};
+  return pino(options, stream);
+};
 
-const logger =
-    process.env.NODE_ENV === "production" ? connectPino() : pino();
+const logger = process.env.NODE_ENV === "production" ? connectPino() : pino();
 
 module.exports = logger;
--- server.js
+++ server.js
@@ -1,41 +1,38 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var expect = require('chai').expect;
-var cors = require('cors');
+var express = require("express");
+var bodyParser = require("body-parser");
+var expect = require("chai").expect;
+var cors = require("cors");
 
-var apiRoutes = require('./routes/api.js');
-var fccTestingRoutes = require('./routes/fcctesting.js');
-var runner = require('./test-runner');
+var apiRoutes = require("./routes/api.js");
+var fccTestingRoutes = require("./routes/fcctesting.js");
+var runner = require("./test-runner");
 var logger = require("./logger");
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({ origin: '*' })); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
+//Routing for API
 apiRoutes(app);
 
 //404 Not Found Middleware
 app.use(function (req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
@@ -43,14 +40,14 @@ app.use(function (req, res, next) {
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   logger.info("Listening on port " + port);
-  if (process.env.NODE_ENV === 'test') {
-    logger.info('Running Tests...');
+  if (process.env.NODE_ENV === "test") {
+    logger.info("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
       } catch (e) {
         var error = e;
-        logger.error('Tests are not valid:');
+        logger.error("Tests are not valid:");
         logger.error(error);
       }
     }, 1500);
